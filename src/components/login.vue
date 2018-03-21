1821a99065be23eff0d567fbe97bd58e6ef9b945
Kakoola <1234@qq.com>
2018-03-21 14:31:27 +0800

    upload

---
+++ src/components/login.vue
@@ -0,0 +1,22 @@
+
+<style scoped>
+  .user>.user-icon{
+    /* background-image:url("assets/logo.png"); */
+    /* background-repeat: no-repeat;
+    background-position: 0px 0px;
+    width: 20px;
+    height: 20px; */
+  }
+</style>
+<template>
+  <div>
+    <h1>登录页面</h1>
+    <div class="user">
+      <li class="user-icon"></li>
+      <input type="text" placeholder="账号">
+    </div>
+
+    <br>
+    <input type="password" placeholder="密码">
+  </div>
+</template>
