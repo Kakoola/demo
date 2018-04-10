708f39133b75a9bf52eb965a66e0d335dd38b155
kakoola <MonsterIsJ@163.com>
2018-04-10 21:24:26 +0800

    add

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,22 +1,44 @@
 
 <style scoped>
-  .user>.user-icon{
-    background-image:url("~assets/logo.png");
-    /* background-repeat: no-repeat;
-    background-position: 0px 0px;
-    width: 20px;
-    height: 20px; */
-  }
+*{
+  margin:0;
+  padding: 0;
+}
+h1{
+  border:1px solid yellow;
+}
+.user {
+  width: 200px;
+  /* border:1px red solid; */
+  position: relative;
+}
+.user-icon {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  top:3px;
+  left:2px;
+  background-image: url("~assets/accout.png");
+  background-repeat: no-repeat;
+  background-position: 0px 0px;
+  /* border: solid 1px blue; */
+}
+.import {
+  padding-left: 20px;
+  border:1px pink solid;
+  height: 20px;
+}
 </style>
 <template>
   <div>
     <h1>登录页面</h1>
     <div class="user">
-      <li class="user-icon"></li>
-      <input type="text" placeholder="账号">
+      <label for="import">
+        <i class="user-icon"></i>
+      </label>
+      <input class="import" type="text" placeholder="帐号">
     </div>
-
     <br>
-    <input type="password" placeholder="密码">
+    <!-- <input type="password" placeholder="密码"> -->
   </div>
 </template>
